2309a809d06a0a0c37d6592374316ac51ec3a969
sahasatvik <sahasatvik@gmail.com>
2016-11-24 20:04:42 +0530

    Fixed whitespace and tabbing

--- math/dectobin.c
+++ math/dectobin.c
@@ -12,17 +12,17 @@ void printbin(Long dec);
 
 /* Converts the numbers in the command line into binary */
 int main(int argc, char const *argv[]) {
-        int i;
-        int size = getnum(numbers, argc, argv);
-        for (i = 0; i < size; i++)
-                printbin(numbers[i]);
-        return 0;
+	int i;
+	int size = getnum(numbers, argc, argv);
+	for (i = 0; i < size; i++)
+		printbin(numbers[i]);
+	return 0;
 }
 
 int getnum(Long *target, int argc, const char *argv[])
 {
-        Long n;
-        int size;
+	Long n;
+	int size;
 	const char *arg;
 	size = 0;
 	while (--argc)
@@ -37,9 +37,9 @@ int getnum(Long *target, int argc, const char *argv[])
 
 void printbin(Long dec)
 {
-        int pow;
-        for (pow = 0; dec >> pow; pow++);
-        for (; pow >= 0; pow--)
-                printf("%d", (int) ((dec >> pow) & 1));
-        printf(" ");
+	int pow;
+	for (pow = 0; dec >> pow; pow++);
+	for (; pow >= 0; pow--)
+		printf("%d", (int) ((dec >> pow) & 1));
+	printf(" ");
 }
